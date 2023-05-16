3bb8be37a56f3f18653d0443354aac0c3affea78
valesalh <valeriansalh@gmail.com>
2023-05-15 18:38:10 -0700

    switched to using react hooks

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import 'tachyons';
 
-// class Card extends Component {
-//     render() {
-//         let { id, name, email } = this.props;
-//         return (
-//             <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-//                 <img alt='robot' src={`https://robohash.org/${id}?200x200`}/>
-//                 <div>
-//                     <h2>{name}</h2>
-//                     <p>{email}</p>
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
+class Card extends React.Component {
+    render() {
+        let { id, name, email } = this.props;
+        return (
+            <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+                <img alt='robot' src={`https://robohash.org/${id}?200x200`}/>
+                <div>
+                    <h2>{name}</h2>
+                    <p>{email}</p>
+                </div>
+            </div>
+        );
+    }
+}
 
 // const Card = (props) => {
 //     let { id, name, email } = props;
@@ -29,16 +29,16 @@ import 'tachyons';
 //     );
 // }
 
-const Card = ({ id, name, email }) => {
-    return (
-        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img alt='robot' src={`https://robohash.org/${id}?200x200`}/>
-            <div>
-                <h2>{name}</h2>
-                <p>{email}</p>
-            </div>
-        </div>
-    );
-}
+// const Card = ({ id, name, email }) => {
+//     return (
+//         <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+//             <img alt='robot' src={`https://robohash.org/${id}?200x200`}/>
+//             <div>
+//                 <h2>{name}</h2>
+//                 <p>{email}</p>
+//             </div>
+//         </div>
+//     );
+// }
 
 export default Card;
